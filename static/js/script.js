97bda0cae03766cc4235b68a50ea16950e55f60a
Bernardo Cecchetto <cecchetto.bernardo@gmail.com>
2021-11-19 12:42:57 -0300

    update 19/11

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,15 @@
-var count=0
 
+var currentUser = "O";
+
+function handleClickEvent(event) {
+  if (event.target.innerHTML.includes("O") || event.target.innerHTML.includes("X")) return;
+
+  event.target.innerHTML = currentUser;
+
+  if (currentUser === "O") {
+    currentUser = "X";
+  } else {
+    currentUser = "O";
+  }
+}
+  
