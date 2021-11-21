94a699ee7f445bdabd28e265aa16deb2384c5682
Bernardo Cecchetto <cecchetto.bernardo@gmail.com>
2021-11-21 17:50:14 -0300

    Update Bernardo 21/11/2021

--- static/js/script.js
+++ static/js/script.js
@@ -1,86 +1,86 @@
 
 var currentUser = "O";
 
-var casas = document.getElementsByTagName('button'); //pega a lista de casas do tabuleiro do jogo
-
-
-
 
 
 //Esta função está atribuindo que o proximo jogador não possa alterar o que o outro jogador preencheu e mudando o preenchimento de texto do botão selecionado
 function handleClickEvent(event) {
+  if (event.target.value.includes("O") || event.target.value.includes("X")) return;
 
-  
+  event.target.value = currentUser;
+  const user = currentUser // fixando o valor do user
 
-  if (event.target.innerHTML.includes("O") || event.target.innerHTML.includes("X" )) return;
-
-  event.target.innerHTML = currentUser;
+  window.setTimeout(() => verify(user), 300) //adicionando um delay de 500s antes de aplicar o verify, para aparecer o resultado antes de verificar quem é o ganhador
 
   if (currentUser === "O") {
     currentUser = "X";
   } else {
     currentUser = "O";
   }
-
- 
+  // Setando qual o jogador, apontando para o innerHTML do h1 com id="currentPlayer"
+  
+  const currentPlayerH1 = document.getElementById("currentPlayer")
+  currentPlayerH1.innerHTML = "Jogador Atual: " + currentUser
 }
 
-function verify(){
-  // Linha 1
-  if (casas[0].innerHTML == casas[1].innerHTML == casas[2].innerHTML != undefined) {
-    window.alert(casas[0].value + casas[1].value + casas[2].value)
+function verify(user) {
+
+  var casas = document.getElementsByTagName('input'); //pega a lista de casas do tabuleiro do jogo
+
+    // Linha 1 - verificar igualdade das casas e, se pelo menos uma for diferente de "", todas as outras são iguais dai
+  if (casas[0].value == casas[1].value && casas[1].value == casas[2].value && casas[0].value !== "") {
+    window.alert('O vencedor é ' + user)
     location.reload()
 
-    // Linha 2
-  } else if (casas[3].innerHTML == casas[4].innerHTML == casas[5].innerHTML != undefined) {
-    window.alert('O vencedor é ' + currentUser)
+    // Linha 2- verificar igualdade das casas e, se pelo menos uma for diferente de "", todas as outras são iguais dai
+  } else if (casas[3].value == casas[4].value && casas[4].value == casas[5].value && casas[3].value !== "") {
+    window.alert('O vencedor é ' + user)
     location.reload()
 
-    // Linha 3
-  } else if (casas[6].innerHTML == casas[7].innerHTML == casas[8].innerHTML != undefined) {
-    window.alert('O vencedor é ' + currentUser)
+    // Linha 3- verificar igualdade das casas e, se pelo menos uma for diferente de "", todas as outras são iguais dai
+  } else if (casas[6].value == casas[7].value && casas[7].value == casas[8].value && casas[6].value!=="") {
+    window.alert('O vencedor é ' + user)
     location.reload()
 
-    // Coluna 1
-  } else if (casas[0].innerHTML == casas[3].innerHTML == casas[6].innerHTML != undefined) {
-    window.alert('O vencedor é ' + currentUser)
+    // Coluna 1- verificar igualdade das casas e, se pelo menos uma for diferente de "", todas as outras são iguais dai
+  } else if (casas[0].value == casas[3].value && casas[3].value == casas[6].value && casas[0].value!== "") {
+    window.alert('O vencedor é ' + user)
     location.reload()
 
-    // Coluna 2
-  } else if (casas[1].innerHTML == casas[4].innerHTML == casas[7].innerHTML != undefined) {
-    window.alert('O vencedor é ' + currentUser)
+    // Coluna 2- verificar igualdade das casas e, se pelo menos uma for diferente de "", todas as outras são iguais dai
+  } else if (casas[1].value == casas[4].value && casas[4].value == casas[7].value && casas[1].value !== "") {
+    window.alert('O vencedor é ' + user)
     location.reload()
 
-    // Coluna 3
-  } else if (casas[2].innerHTML == casas[5].innerHTML == casas[8].innerHTML != undefined) {
-    window.alert('O vencedor é ' + currentUser)
+    // Coluna 3- verificar igualdade das casas e, se pelo menos uma for diferente de "", todas as outras são iguais dai
+  } else if (casas[2].value == casas[5].value && casas[5].value == casas[8].value && casas[2].value !== "") {
+    window.alert('O vencedor é ' + user)
     location.reload()
 
-    // Diagonal 1
-  } else if (casas[0].innerHTML == casas[4].innerHTML == casas[8].innerHTML != undefined) {
-    window.alert('O vencedor é ' + currentUser)
+    // Diagonal 1- verificar igualdade das casas e, se pelo menos uma for diferente de "", todas as outras são iguais dai
+  } else if (casas[0].value == casas[4].value && casas[4].value == casas[8].value && casas[0].value!== "") {
+    window.alert('O vencedor é ' + user)
     location.reload()
 
-    // Diagonal 3
-  } else if (casas[2].innerHTML == casas[4].innerHTML == casas[6].innerHTML != undefined) {
-    window.alert('O vencedor é ' + currentUser)
+    // Diagonal 2- verificar igualdade das casas e, se pelo menos uma for diferente de "", todas as outras são iguais dai
+  } else if (casas[2].value == casas[4].value && casas[4].value == casas[6].value && casas[2].value !== "") {
+    window.alert('O vencedor é ' + user)
     location.reload()
   }
+  // Se todas forem preenchidas, mas nenhuma for igual, deu velha
   else if (
-    casas[0].value != undefined &&
-    casas[1].value != undefined &&
-    casas[2].value != undefined &&
-    casas[3].value != undefined &&
-    casas[4].value != undefined &&
-    casas[5].value != undefined &&
-    casas[6].value != undefined &&
-    casas[7].value != undefined &&
-    casas[8].value != undefined
+    casas[0].value !== "" &&
+    casas[1].value !== "" &&
+    casas[2].value !== "" &&
+    casas[3].value !== "" &&
+    casas[4].value !== "" &&
+    casas[5].value !== "" &&
+    casas[6].value !== "" &&
+    casas[7].value !== "" &&
+    casas[8].value !== ""
   ) {
     window.alert('DEU VELHA! ')
     location.reload()
-  } else {
-  pass
   }
 }
 
